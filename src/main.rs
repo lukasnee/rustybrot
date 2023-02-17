6c28578f3f54f736dfc7961f9b1cca2180af6c89
lukasnee <lukas.neverauskis@gmail.com>
2023-02-17 21:14:49 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
 mod complex;
 use complex::Complex;
 
-const MAX_ITERATIONS: u64 = 30;
-const SCREEN_SIZE: u64 = 400;
-const VIEW_SPAN_X: f64 = 4.0;
-const VIEW_SPAN_Y: f64 = 4.0;
-
-// const PALLETE: &str = "%#*-.";
-
-fn get_iterations(c: &Complex) -> u64 {
+fn get_iterations(c: &Complex, max_iterations_last_avg: u64) -> u64 {
     let mut z_curr = Complex {
         real: 0.0,
         imag: 0.0,
     };
-    for x in 1..MAX_ITERATIONS {
+    for x in 1..max_iterations_last_avg {
         z_curr = z_curr.mul(&z_curr).add(&c);
-        if z_curr.modulus() > 2.0 {
+        if z_curr.modulus() > 4.0 {
             return x;
         }
     }
-    MAX_ITERATIONS
+    max_iterations_last_avg
 }
 
 fn map(
@@ -34,29 +27,93 @@ fn map(
         + output_min
 }
 
-fn print_line(x_steps: u64, imag: f64) {
-    let pallete: Vec<char> = vec!['%', '#', '*', '-', '.']; // TODO optimze
+fn print_line(
+    real_offset: f64,
+    real_span: f64,
+    real_steps: u64,
+    imag: f64,
+    max_iterations_last_avg: u64,
+) {
+    let pallete: Vec<char> = vec!['@', '#', '%', '+', '*', '-', '.', '`', ' ']; // TODO optimize
 
-    let step_size = VIEW_SPAN_X / (x_steps as f64);
-    for step in 0..(x_steps + 1) {
+    let step_size = real_span / (real_steps as f64);
+    for step in 0..(real_steps + 1) {
         let c = Complex {
-            real: ((step as f64) * step_size) - (VIEW_SPAN_X / 2.0),
+            real: ((step as f64) * step_size) - (real_span / 2.0) + real_offset,
             imag,
         };
-        let iterations = get_iterations(&c);
-        let color =
-            map(iterations, 1, MAX_ITERATIONS, 0, (pallete.len() as u64) - 1)
-                as usize;
-        print!("{}", pallete[color]);
+        let iterations = get_iterations(&c, max_iterations_last_avg);
+        let color = map(
+            iterations,
+            1,
+            max_iterations_last_avg,
+            0,
+            (pallete.len() as u64) - 1,
+        ) as usize;
+        print!("{}", pallete[pallete.len() - 1 - color]);
     }
-    println!();
+    // println!();
 }
 
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
 fn main() {
-    let step_size = VIEW_SPAN_Y / (SCREEN_SIZE as f64);
-    for step in 0..(SCREEN_SIZE + 1) {
-        let imag = ((step as f64) * step_size) - (VIEW_SPAN_Y / 2.0);
-        // println!("{}", imag);
-        print_line(SCREEN_SIZE * 2, imag);
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    // let mut stdout = stdout();
+
+    let mut real_offset: f64 = 0.0;
+    let mut imag_offset: f64 = 0.0;
+    let mut view_size: f64 = 4.0;
+    let mut max_iterations_last_avg: u64 = 100;
+    const max_iterations: u64 = 30;
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Char('q') => view_size += (view_size / 10.0),
+            Key::Char('w') => imag_offset -= (view_size / 10.0),
+            Key::Char('e') => view_size -= (view_size / 10.0),
+            Key::Char('a') => real_offset -= (view_size / 10.0),
+            Key::Char('s') => imag_offset += (view_size / 10.0),
+            Key::Char('d') => real_offset += (view_size / 10.0),
+            Key::Char('1') => {
+                max_iterations_last_avg +=
+                    (((max_iterations_last_avg as f64) / 10.0) as u64)
+            }
+            Key::Char('2') => {
+                max_iterations_last_avg -=
+                    (((max_iterations_last_avg as f64) / 10.0) as u64)
+            }
+
+            Key::Ctrl('z') => break,
+            _ => (),
+        }
+
+        let (terminal_width, terminal_height) =
+            termion::terminal_size().unwrap();
+        let aspect_ratio = (terminal_width as f64) / (terminal_height as f64);
+
+        let real_span = view_size;
+        let imag_span = view_size / aspect_ratio;
+
+        let step_size = imag_span / ((terminal_height / 2) as f64);
+        for step in 0..((terminal_height) + 1) {
+            let imag =
+                ((step as f64) * step_size) - (imag_span / 2.0) + imag_offset;
+            // println!("{}", imag);
+            print_line(
+                real_offset,
+                real_span,
+                (terminal_width - 1).into(),
+                imag,
+                max_iterations_last_avg,
+            );
+        }
+
+        write!(stdout, "{}", termion::cursor::Goto(1, 1)).unwrap();
+        write!(stdout, "{}", termion::clear::All).unwrap();
     }
 }
